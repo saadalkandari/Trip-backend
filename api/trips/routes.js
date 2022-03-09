d1ffc8c08bd3bfcb93f15af76df11dc6b6c90a4b
SaadAlkandari <mr.s.alkandari@gmail.com>
2022-03-09 19:23:34 +0300

    deleteTrip dont

--- api/trips/routes.js
+++ api/trips/routes.js
@@ -1,8 +1,21 @@
 const express = require("express");
-const { fetchTrips } = require("./controllers");
+const { fetchTrips, deleteTrip, fetchTrip } = require("./controllers");
 
 const router = express.Router();
 
+router.param("tripId", async (req, res, next, tripId) => {
+  const trip = await fetchTrip(tripId, next);
+  if (trip) {
+    req.trip = trip;
+    next();
+  } else {
+    const err = new Error("Trip Not Found");
+    err.status = 404;
+    next(err);
+  }
+});
+
 router.get("/", fetchTrips);
+router.delete("/:tripId", deleteTrip);
 
 module.exports = router;
